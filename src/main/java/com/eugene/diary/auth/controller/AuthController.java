30bd5cdef922367e093d750360e0d53bba41ed56
jyj1289 <jin37091289@gmail.com>
2025-01-03 18:39:51 +0900

    feat: CommonResponse 생성

--- src/main/java/com/eugene/diary/auth/controller/AuthController.java
+++ src/main/java/com/eugene/diary/auth/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.eugene.diary.auth.controller;
 
 import com.eugene.diary.auth.controller.dto.response.TokenResponse;
 import com.eugene.diary.auth.service.GoogleAuthService;
+import com.eugene.diary.shared.response.CommonResponse;
+import com.eugene.diary.shared.response.SingleCommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +15,16 @@ public class AuthController {
     private final GoogleAuthService googleAuthService;
 
     @GetMapping("/google/link")
-    public String getGoogleAuthUrl() {
-        return googleAuthService.getGoogleAuthLink();
+    public SingleCommonResponse<String> getGoogleAuthUrl() {
+        return CommonResponse.success(
+                googleAuthService.getGoogleAuthLink()
+        );
     }
 
     @PostMapping("/google")
-    public TokenResponse authWithGoogle(@RequestParam String code) {
-        return googleAuthService.googleAuth(code);
+    public SingleCommonResponse<TokenResponse> authWithGoogle(@RequestParam String code) {
+        return CommonResponse.success(
+            googleAuthService.googleAuth(code)
+        );
     }
 }
